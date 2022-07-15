2b92f06130bf3e40731c6297a779762f0e49e415
Roca Carlos <rocaedono@gmail.com>
2022-07-15 23:33:25 +0900

    finally the first beta

--- hangman_game2.py
+++ hangman_game2.py
@@ -1,9 +1,9 @@
 from random import choice 
 import os
 
-def read_list(type_game):
+def read_list(game_mode):
     file_type = {1:"animals.txt", 2:"family.txt", 3:"food.txt" }
-    file_selected = file_type[type_game]
+    file_selected = file_type[game_mode]
     with open(file_selected, 'r') as f:
         game_word_tuple =  f.readline()
         game_word_tuple = tuple(map(lambda x: x.replace('\n', ''), game_word_tuple))
@@ -17,7 +17,7 @@ def change_word(old_word, letter, letter_index):
 
 
 def screen(screen_type, life, points, word_line, msj):
-    game_screens = {99:'screen_title.txt', 98:'screen_game_over.txt', 1:'hang_1.txt', 2:'hang_2.txt', 3:'hang_3.txt', 4:'hang_4.txt', 5:'hang_5.txt', 6:'hang_6.txt', 7:'hang_7.txt', 8:'hang_8.txt', 9:'hang_9.txt'}
+    game_screens = {99:'screen_title.txt', 98:'screen_game_over.txt', 97:'screen_victory.txt', 96:'screen_ endgame.txt', 1:'hang_1.txt', 2:'hang_2.txt', 3:'hang_3.txt', 4:'hang_4.txt', 5:'hang_5.txt', 6:'hang_6.txt', 7:'hang_7.txt', 8:'hang_8.txt', 9:'hang_9.txt'}
     game_screen_display = game_screens[screen_type]
     screen_settings = {'??':str(life), '$$':str(points), '>>':word_line, '<<':msj}
     with open(game_screen_display, 'r') as f:
@@ -26,46 +26,126 @@ def screen(screen_type, life, points, word_line, msj):
     return screen_displaying
 
 
-def game_over():
-    pass
-
-
-def choose_word():
-    pass 
-
-
+def retry():
+    while True:
+        try:
+            retry = input()
+            assert retry == 1 or 2
+        except:
+            msj = 'Wrong input, select 1 or 2:\n Do you want to try again?\nYES = 1\nNO = 2'
+            continue
+        else:
+            break
+    if retry == 1:
+        screen_type = 99
+    elif retry == 2:
+        screen_type = 96
+    return screen_type
 
 
 
 def bootgame():
+    game_run = True
+    life = 3
+    points = 0
+    screen_type = 0
+    game_mode = 0
+    figure_out_word = ''
+    word_line = ''
+    msj = ''
+    choose_letter = ''
+    used_letters = []
+    mistake = ['No', 'Nope', 'Not even close', 'Good try, but nop']
+    find_msj = ['Good, you found one', 'Yeah, you are more close to win', 'Found one, continue like this', 'Your election was correct']
     
-    while life > 0:
-        print(screen())
-        #selected ramdom/make word to figure out
-        
-        #screen 1
-        #choose words
-        # if good 
-        #   -same screen, appear letter,
-        #   choose word
-        #if bad
-        #   next screem
-                #minus points
-        #   choose word
-        gametype = input()
+    while game_run == True:
+        if life > 0:
+            screen_type = 99                                        #####
+            os.system("clear")
+            print(screen(screen_type, life, points, word_line, msj))
+            msj = ''                                                #####
+            try:
+                game_mode = input()
+                assert game_mode == 1 or 2 or 3
+            except:
+                msj = 'Wrong input, select 1, 2 or 3:'
+                continue
+            else:
+                figure_out_word = choice(read_list(game_mode))
+                word_line = '_' * len(figure_out_word)
+                screen_type = 1                
+            while '_' in word_line and screen_type != 7:   ################
+                os.system("clear")
+                print(screen(screen_type, life, points, word_line, msj))
+                try:
+                    choose_letter = str(input())
+                    choose_letter = choose_letter.upper() 
+                    assert choose_letter == ('A' <= choose_letter)  or (choose_letter <= 'Z') and choose_letter.isalpha()
+                except:    
+                    choose_letter = ''
+                    msj = 'please choose only one letter from A to Z'
+                    continue 
+                else:
+                    if choose_letter in used_letters:
+                        msj = 'you already used that letter'
+                        screen_type += 1 
+                    elif not choose_letter in figure_out_word:
+                        msj = choice(mistake) + 'try again'
+                        screen_type += 1
+                    else:
+                        used_letters.append(choose_letter)
+                        while choose_letter in figure_out_word:
+                            letter_index = figure_out_word.find(choose_letter)
+                            word_line = change_word(word_line, choose_letter, letter_index)
+                            figure_out_word = change_word(figure_out_word, choose_letter, letter_index)
+                            msj = choice(find_msj)
+            if '_' not in word_line:
+                points += 1
+                msj = ''
+                screen_type = 97
+                os.system("clear")
+                print(screen(screen_type, life, points, word_line, msj))
+                screen_type = retry()
+            elif screen_type == 7 and life >1:
+                life -=1 
+                msj = ''
+                screen_type = 98                                        
+                os.system("clear")
+                print(screen(screen_type, life, points, word_line, msj))
+                screen_type = retry()
+        elif life == 0:
+            screen_type = 96                                        
+            os.system("clear")
+            print(screen(screen_type, life, points, word_line, msj))
+                
 
-    # retry screen
-    #if yes 
-        #life points +1
-        # bootgame():
-    #elif no        
-        #game_over()
+                           
+
+                
+          
+            
+            #screen 1
+            #choose words
+            # if good 
+            #   -same screen, appear letter,
+            #   choose word
+            #if bad
+            #   next screem
+                    #minus points
+            #   choose word
+            
+    #    else:
+            # retry screen
+            #if yes 
+                #life points +1
+                # bootgame():
+            #elif no        
+    #print(game_over())
+                
 
 
-points = 0
-attemps = 0
-life = 3
-screen_type = 99
+
+
 
 
 
